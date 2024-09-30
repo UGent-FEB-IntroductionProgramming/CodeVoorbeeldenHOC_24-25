259016ac2b0cec36962c201a3ed2df29026645c6
Frederik Gailly <Frederik.Gailly@ugent.be>
2024-10-01 01:07:35 +0200

    Code Les 2

---
+++ Les2/opp_cirkel.py
@@ -0,0 +1,11 @@
+
+#print("Geef een waarde voor de straal?")
+#straal_txt = input()
+straal_txt = input("Geef een waarde voor de straal?")
+straal = float(straal_txt)
+PI = 3.141592653589793
+
+opp = PI * straal * straal
+
+print("De opp van de cirkel is  "+ str(opp))
+print("De opp van de cirkel is", opp)
